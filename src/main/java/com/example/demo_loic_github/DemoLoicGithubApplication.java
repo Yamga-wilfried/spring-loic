49939c9cf56981f9d21bfab960ee33164c2b115b
Loïc Wilfried Yamga Ngeulieutou <118023980+Yamga-wilfried@users.noreply.github.com>
2026-02-06 00:15:32 +0100

    Update DemoLoicGithubApplication.java

--- src/main/java/com/example/demo_loic_github/DemoLoicGithubApplication.java
+++ src/main/java/com/example/demo_loic_github/DemoLoicGithubApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class DemoLoicGithubApplication { // J'ai ajouté "Loic" ici pour correspondre au fichier
+public class DemoLoicGithubApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoLoicGithubApplication.class, args);
