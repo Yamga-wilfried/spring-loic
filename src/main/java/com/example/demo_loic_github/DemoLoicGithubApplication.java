cdafb438085e9e89e6fe5d9495416b438f34ac22
Yamga-wilfried <wilfriedyamga@gmail.com>
2026-02-05 10:27:07 +0100

    first commit

---
+++ src/main/java/com/example/demo_loic_github/DemoLoicGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_loic_github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoLoicGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoLoicGithubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_loic_github/DemoLoicGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_loic_github;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoLoicGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
